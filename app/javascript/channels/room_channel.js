709c5fd294d5fc97391151ce31dca605956d918e
Kristina <codingkris@yahoo.com>
2020-05-20 12:07:56 +0300

    send_message job created

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -11,6 +11,18 @@ consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2 }, {
   },
 
   received(data) {
-    console.log(data);
+    const user_element = document.getElementById('user-id');
+    const user_id = Number(user_element.getAttribute('data-user-id'));
+
+    let html;
+
+    if (user_id === data.message.user_id) {
+      html = data.mine
+    } else {
+      html = data.theirs
+    }
+
+    const messageContainer = document.getElementById('messages')
+    messageContainer.innerHTML = messageContainer.innerHTML + html
   }
 });
